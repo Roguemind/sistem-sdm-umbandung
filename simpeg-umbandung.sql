dff65a8f7bb03c8c0ab343a3f3f91056e965eb42
Roguemind <m.faisal.n12@gmail.com>
2022-12-07 13:13:10 +0700

    Update DB

--- simpeg-umbandung.sql
+++ simpeg-umbandung.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2022 at 06:55 AM
+-- Generation Time: Dec 07, 2022 at 07:12 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
